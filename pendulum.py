4daeebc54f5ffcdf82e3d7543779d238a09687d6
inoma <syookiti@gmail.com>
2021-05-30 23:02:03 +0900

    13章追加(未完)

--- pendulum.py
+++ pendulum.py
@@ -4,12 +4,13 @@ import matplotlib.animation as animation
 
 
 class InvertedPendulum(object):
-    M = 0.8
+    M = 0.7
     m = 0.12
-    l = 0.5
+    l = 0.3
+    J = 9e-3
     g = 9.8
     t = 0.1
-    t_num = 1000
+    t_num = 120
 
     def __init__(self, x, theta, noisy=True):
         self.x = x
@@ -30,28 +31,31 @@ class InvertedPendulum(object):
         return (self.theta, self.theta_dot)
 
     def update_state(self):
+        ml = self.m * self.l
+        Mt = self.M + self.m
+        Jt = self.J + ml*self.l
+
         for i in range(self.t_num):
             costheta = np.cos(self.theta)
             sintheta = np.sin(self.theta)
-            ml = self.m * self.l
-            total_mass = self.M + self.m
+            alpha = Mt*Jt - (ml*costheta)**2
 
-            temp = (self.u + ml * self.theta_dot**2 * sintheta) / total_mass
-            thetaacc = ((self.g * sintheta - costheta * temp) /
-                        (self.l * (4/3 - self.m * costheta**2 / total_mass)))
-            xacc = temp - ml * thetaacc * costheta / total_mass
+            thetaacc = ( ml*sintheta*( Mt*self.g - ml*self.theta_dot**2*costheta ) - self.u*ml*costheta ) / alpha
+            xacc = - ( ml*sintheta*( ml*self.g*costheta - Jt*self.theta_dot**2 ) - self.u*Jt ) / alpha
 
             self.x += self.t_one * self.x_dot
             self.x_dot += self.t_one * xacc
             self.theta += self.t_one * self.theta_dot
             self.theta_dot += self.t_one * thetaacc
 
-
     def get_car_x(self):
         return self.x
 
+    def get_state(self):
+        return self.x, self.theta, self.x_dot, self.theta_dot
 
-def video(x_history, angle_history, l, t):
+
+def video(x_history, angle_history, l, t, animation_save=False):
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
     ax.grid()
@@ -68,11 +72,14 @@ def video(x_history, angle_history, l, t):
                       [0, 2*l*np.cos(angle_history[i])])
         time_text.set_text('time = {0:.1f}'.format(i*t))
         return line, time_text
-
+    
     ani = animation.FuncAnimation(fig, animate, frames=range(len(x_history)),
                                   interval=1000*t, blit=False, init_func=init)
-    ani.save("out.gif", writer = 'imagemagick')
-    plt.show()
+
+    if animation_save:
+        ani.save("./test_out.gif", writer = 'imagemagick')
+    else:
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -84,8 +91,9 @@ if __name__ == '__main__':
 
     for i, u in enumerate(u_list):
         next_s = plant.do_action(u)
-        print(next_s, next_s[0]*180/np.pi, plant.get_car_x())
+        #print(next_s, next_s[0]*180/np.pi, plant.get_car_x())
         angle_history.append(next_s[0])
         x_history.append(plant.get_car_x())
 
+    print(angle_history)
     video(x_history, angle_history, plant.l, plant.t)
